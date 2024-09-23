7d0d2ea3edf984c9a0a5bf3a5d15c26276c4273b
Megatron Procoderr <harshitmeenaiitb1@gmail.com>
2024-09-24 04:22:19 +0530

    implement latest() method for snippetModel struct

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"database/sql"
+	"fmt"
 	"megatroncodrr/snippetbox/pkg/models"
 )
 
@@ -38,8 +39,34 @@ func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 	} else if err != nil {
 		return nil, err
 	}
+
 	return s, nil
 }
 
 // return 10 most recently created snippet
-func (*SnippetModel) Latest() {}
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+	stmt := `SELECT id, title, content , created, expires
+	FROM snippets
+	WHERE expires > UTC_TIMESTAMP ORDER BY created DESC LIMIT 10`
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	snippets := []*models.Snippet{}
+	for rows.Next() {
+		s := &models.Snippet{}
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	fmt.Printf("rows : \t %v", snippets)
+	return snippets, nil
+}
